43ae652810a9685df32cdf0266c89914c90cf04a
ferchoclimber <107957048+ferchoclimber@users.noreply.github.com>
2024-06-01 13:01:02 -0600

    se modifico, la estructure general de pagina index .html, se incerto header con tailwing

---
+++ pages/api/calls.js
@@ -0,0 +1,8 @@
+// import { MongoClient } from "mongodb";
+// export default async function handler (req, res) {
+//     const {method, body, query}= req;
+//     //recordemos que hacer esto es el equivalente a 
+//     //const method=req.method;
+//     //const body=req.body;
+//     //const query=req.query;
+// }
