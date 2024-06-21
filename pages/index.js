a0c970524961fe5f4a8b006a2f39cef86affde4f
ferchoclimber <107957048+ferchoclimber@users.noreply.github.com>
2024-06-20 18:16:31 -0600

    se pudo terminar ejecutar el codigo con npm run bu ild

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function App() {
+function TruchaClimbing() {
   return (
     <>
       <header>
         <div className="header__logo">
-          <img src="img/trucha.jpeg" width="120" height="120" alt="Trucha Climbing Logo" />
+          <img src="img/trucha.jpeg" width="120" height="120" alt="Trucha Climbing" />
           <a className="luckiest_guy_regular">Trucha!!!! Climbing</a>
         </div>
         <div className="header_nav">
@@ -20,96 +20,78 @@ function App() {
       </header>
 
       <section id="logo">
-        <div id="default-carousel" className="relative w-full" data-carousel="slide">
-          <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-            <div className="hidden duration-700 ease-in-out" data-carousel-item>
-              <img src="img/2017050909135822319.jpg" width="800" height="600" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-            </div>
-            <div className="hidden duration-700 ease-in-out" data-carousel-item>
-              <img src="img/IMG_0585.JPG" width="1200" height="600" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-            </div>
-            <div className="hidden duration-700 ease-in-out" data-carousel-item>
-              <img src="img/nm2.jpg" width="700" height="600" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-            </div>
-          </div>
-          <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-            <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-            <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-            <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-          </div>
-          <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-              <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
-              </svg>
-              <span className="sr-only">Previous</span>
-            </span>
-          </button>
-          <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-              <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
-              </svg>
-              <span className="sr-only">Next</span>
-            </span>
-          </button>
+        <div className="slider-box">
+          <ul>
+            <li>
+              <img src="img/2017050909135822319.jpg" height="500" width="600" alt="Imagen 1" />
+              <div className="texto">
+                <h2>imagen 1</h2>
+                <p>textodjñjsñjfñsjñsjñsjgjgñsjgñsjgñsjgñsgjñwjgñsjñ</p>
+              </div>
+            </li>
+            <li>
+              <img src="img/IMG_0585.JPG" height="500" width="800" alt="Imagen 2" />
+              <div className="texto">
+                <h2>imagen 2</h2>
+                <p>textodjñjsñjfñsjñsjñsjgjgñsjgñsjgñsjgñsgjñwjgñsjñ</p>
+              </div>
+            </li>
+            <li>
+              <img src="img/nm2.jpg" height="500" width="800" alt="Imagen 3" />
+              <div className="texto">
+                <h2>imagen 3</h2>
+                <p>textodjñjsñjfñsjñsjñsjgjgñsjgñsjgñsjgñsgjñwjgñsjñ</p>
+              </div>
+            </li>
+          </ul>
         </div>
       </section>
 
-      <section id="barraMedia">
-        <center>
-          <p>Atrevete a vivir la experiencia</p>
-          <video src="img/video2.mp4" controls width="600" height="500" />
-        </center>
+      <section id="barraMedia" style={{ textAlign: 'center' }}>
+        <p>Atrevete a vivir la experiencia</p>
+        <video src="img/video2.mp4" controls width="600" height="500" />
       </section>
 
       <section id="tienda">
         <div className="carta">
           <h3>Escalada en chihuahua</h3>
-          <p>Puedes acceder a la informacion de todas las zonas de escalada que hay en chihuahua, ademas de la historia de cada una de la zonas, sobre quien las desarrollo y que año se empezo a escalar ahi.</p>
+          <p>Puedes acceder a la informacion de todas las zonas de escalada que hay en chihuahua, ademas de la historia de cada una de las zonas, sobre quien las desarrollo y que año se empezo a escalar ahi.</p>
           <a href="escaladaChih.html"><button>Ver</button></a>
         </div>
-
         <div className="carta">
           <h3>Donaciones</h3>
-          <p>Accede a la cuenta comun para que puedas apoyar la escalada en Chihuahua, las colaboraciones seran destinadas a continuar con el desarrollo de vias nuevas o en su defecto en la reequipacion de las vias existentes. </p>
+          <p>Accede a la cuenta comun para que puedas apoyar la escalada en Chihuahua, las colaboraciones seran destinadas a continuar con el desarrollo de vias nuevas o en su defecto en la reequipacion de las vias existentes.</p>
           <a href="donaciones.html"><button>Ver</button></a>
         </div>
-
         <div className="carta">
           <h3>Cursos</h3>
-         
-     <p>Quieres aprender a escalar? haz click para acceder a cursos de escalada, vive la experencia 
-     de este hermoso deporte y agenda una clase muestra.
-     </p>
-     <a href="cursos.html"><button>Ver</button></a>
-    </div>
-  </section>
-
-  <footer>
-    <div className="container2">
-        <div className="logo2">
-            <img src="img/trucha.jpeg" width="100" height="100" alt="Footer Logo" />
+          <p>Quieres aprender a escalar? haz click para acceder a cursos de escalada, vive la experiencia de este hermoso deporte y agenda una clase muestra.</p>
+          <a href="cursos.html"><button>Ver</button></a>
         </div>
-        <div className="columnas">
+      </section>
+
+      <footer>
+        <div className="container2">
+          <div className="logo2">
+            <img src="img/trucha.jpeg" width="100" height="100" alt="Trucha Logo" />
+          </div>
+          <div className="columnas">
             <div className="columna">
               <h4>COLABORACIONES</h4>
-              <ul>              
+              <ul>
                 <li><a href="#">IKA climbing gym</a></li>
                 <li><a href="#">Larva</a></li>
                 <li><a href="#">Sayab Centro deportivo</a></li>
               </ul>
             </div>
-
             <div className="columna">
               <h4>GRUPOS SOCIALES</h4>
-              <ul>            
+              <ul>
                 <li><a href="#">Viva la rock</a></li>
                 <li><a href="#">Club Alpino</a></li>
                 <li><a href="#">La beta chica</a></li>
               </ul>
             </div>
-
             <div className="columna">
               <h4>MARCAS PRINCIPALES</h4>
               <ul>
@@ -120,19 +102,17 @@ function App() {
                 <li><a href="#">La sportiva</a></li>
               </ul>
             </div>
+          </div>
+          <div className="redes-sociales">
+            <a href="#"><i className='bx bxl-facebook-circle'></i></a>
+            <a href="#"><i className='bx bxl-instagram-alt'></i></a>
+            <a href="#"><i className='bx bxl-twitter'></i></a>
+            <a href="#"><i className='bx bxl-youtube'></i></a>
+          </div>
         </div>
-        <div className="redes-sociales">
-          <a href="#"><i className='bx bxl-facebook-circle'></i></a>
-          <a href="#"><i className='bx bxl-instagram-alt'></i></a>
-          <a href="#"><i className='bx bxl-twitter'></i></a>
-          <a href="#"><i className='bx bxl-youtube'></i></a>
-        </div>
-    </div>
-  </footer>
-
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
-</>
+      </footer>
+    </>
   );
 }
 
-export default App;
+export default TruchaClimbing;
